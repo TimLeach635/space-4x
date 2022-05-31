4078512357929f99db90b67f3790832c591fe172
Tim Leach <timleach635@gmail.com>
2022-05-31 12:35:46 +0100

    Add orbit animation
    Add a "moon" object, and add an "update" method to the scene to modify
    its location based on the current game time

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,32 @@ import Phaser from "phaser";
 
 class MyScene extends Phaser.Scene
 {
+  planet: Phaser.GameObjects.Arc;
+  moon: Phaser.GameObjects.Arc;
+
   preload = () => {
     
   };
 
   create = () => {
-    const circle = this.scene.scene.add.arc(400, 300, 100, 0, 360, false, 0xffffff);
+    this.planet = this.add.arc(400, 300, 80, 0, 360, false, 0xaaffff);
+    this.moon = this.add.arc(400, 300, 20, 0, 360, false, 0xffffff);
   };
+
+  updateMoonPosition(time: number): void {
+    const orbitRadius = 120;
+    const orbitCentreX = 400;
+    const orbitCentreY = 300;
+    const tilt = 0.7;  // should range from 0 to 1
+    const period = 5000;  // in milliseconds
+
+    this.moon.x = orbitCentreX + orbitRadius * Math.sin(time * 2 * Math.PI / period);
+    this.moon.y = orbitCentreY + orbitRadius * Math.cos((time * 2 * Math.PI / period) + (tilt * Math.PI / 2));
+  }
+
+  update(time: number, delta: number): void {
+    this.updateMoonPosition(time);
+  }
 }
 
 window.onload = () => {
