dd0c1427091a1d3bd14e8b6c0f25ad21349f1ae2
Tim Leach <timleach635@gmail.com>
2022-05-31 14:08:47 +0100

    Modify graphics and animation
    Add pixel art sprites, and add draw sorting to the moon's orbit
    animation so it goes behind the earth

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,38 @@ import Phaser from "phaser";
 
 class MyScene extends Phaser.Scene
 {
-  planet: Phaser.GameObjects.Arc;
-  moon: Phaser.GameObjects.Arc;
+  background: Phaser.GameObjects.Image;
+  earth: Phaser.GameObjects.Image;
+  moon: Phaser.GameObjects.Image;
 
   preload = () => {
-    
+    this.load.image("space-bg", "assets/space-bg.png");
+    this.load.image("earth", "assets/earth.png");
+    this.load.image("moon", "assets/moon.png");
   };
 
   create = () => {
-    this.planet = this.add.arc(400, 300, 80, 0, 360, false, 0xaaffff);
-    this.moon = this.add.arc(400, 300, 20, 0, 360, false, 0xffffff);
+    this.background = this.add.image(200, 150, "space-bg");
+    this.background.depth = -100;
+    this.earth = this.add.image(200, 150, "earth");
+    this.moon = this.add.image(200, 150, "moon");
   };
 
   updateMoonPosition(time: number): void {
-    const orbitRadius = 120;
-    const orbitCentreX = 400;
-    const orbitCentreY = 300;
+    const orbitRadius = 60;
+    const orbitCentreX = 200;
+    const orbitCentreY = 150;
     const tilt = 0.7;  // should range from 0 to 1
     const period = 5000;  // in milliseconds
 
     this.moon.x = orbitCentreX + orbitRadius * Math.sin(time * 2 * Math.PI / period);
     this.moon.y = orbitCentreY + orbitRadius * Math.cos((time * 2 * Math.PI / period) + (tilt * Math.PI / 2));
+
+    if (this.moon.x - orbitCentreX < orbitCentreY - this.moon.y) {
+      this.moon.depth = -1;
+    } else {
+      this.moon.depth = 1;
+    }
   }
 
   update(time: number, delta: number): void {
@@ -34,8 +45,10 @@ window.onload = () => {
   let game: Phaser.Game;
 
   game = new Phaser.Game({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 300,
+    zoom: 2,
+    pixelArt: true,
     type: Phaser.AUTO,
     parent: "content",
     title: "Space 4X",
